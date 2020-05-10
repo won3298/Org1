c0e946b6b93e59897534e5907185fee00d8ed0c8
SeongWon <won3298@gmail.com>
2020-05-11 08:57:26 +0900

    test

--- src/aura/orgTest/orgTestController.js
+++ src/aura/orgTest/orgTestController.js
@@ -2,4 +2,34 @@
  * Created by MS on 2020-05-06.
  */
 ({
+    doCreate : function (component, event, helper){
+        console.clear();
+        let con = component.get('v.con');
+        console.log(con.LastName);
+
+
+
+        helper.apex(
+            component, 'doCreate','create', {'con':con}
+        ).then(function (resData,response){
+                 helper.gfn_toast('정상적으로 가입되었습니다.','s');
+                 console.log('성공');
+                 $A.enqueueAction(component.get('c.doCancel'));
+                        helper.gfn_refresh();
+                    }).catch(function (error, response) {
+                        helper.gfn_ApexErrorHandle(error, response);
+                        console.log("fail");
+                    });
+
+        },
+
+
+
+ doCancel : function(component,event,helper){
+            helper.gfn_closeQuickAction(component);
+            console.log("nice");
+        }
+
+
+
 })
